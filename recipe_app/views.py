36b104b06e313c1f7810e6e89637977bccefcf54
cbalbera <caleb.balbera@gmail.com>
2022-08-02 08:56:33 -0600

    updated search to separate each individual element

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -11,15 +11,22 @@ from recipe_app.models import recipe
 def say_hello(request):
     return render(request, 'hello.html',{"name":"caleb"})
 
+# mapped to /home
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+# mapped to /search
 class SearchResultsView(ListView):
     model = recipe
     template_name = 'search_results.html'
-    def get_queryset(self): # new
-        query = self.request.GET.get("q")
-        object_list = recipe.objects.filter(
-            Q(name__icontains=query) | Q(cuisine__icontains=query)
-        )
+    def get_queryset(self):
+        name_query = self.request.GET.get("name")
+        print("name is "+name_query)
+        cuisine_query = self.request.GET.get("cuisine")
+        print("cuisine is "+cuisine_query)
+        course_query = self.request.GET.get("course")
+        print("course is is "+course_query)
+        object_list = recipe.objects.filter(Q(name__icontains=name_query)
+        ).filter(Q(cuisine__icontains=cuisine_query)
+        ).filter(Q(course__icontains=course_query))
         return object_list
